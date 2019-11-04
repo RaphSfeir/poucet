b7ad16f584f2fedb9d2b87dc9307bc69103ace23
Raphaël Sfeir <raph.sfeir@gmail.com>
2019-11-04 16:13:28 +0000

    Added Hex publishing infos for package

--- mix.exs
+++ mix.exs
@@ -7,21 +7,42 @@ defmodule Poucet.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: description(),
+      name: "Petit Poucet",
+      source_url: "https://github.com/RaphSfeir/poucet",
+      homepage_url: "https://github.com/RaphSfeir/poucet",
+      docs: [
+        main: "Poucet",
+        extras: ["README.md"]
+      ]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  defp description() do
+    "Very simple tool to get the final destination of a potential redirects URL."
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{
+        "Github Page" => "https://github.com/RaphSfeir/poucet"
+      }
+    ]
+  end
+
   defp deps do
     [
-      {:httpoison, "~> 1.6"}
+      {:httpoison, "~> 1.6"},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
     ]
   end
 end
